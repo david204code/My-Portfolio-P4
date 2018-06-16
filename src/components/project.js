390521d9176b79ad705080d80921e45cbfda58a3
David Wu <david204code@gmail.com>
2018-06-16 23:16:47 +0100

    created project file components

---
+++ src/components/project.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './project.css';
+
+class Project extends React.Component {
+    render() {
+        return(
+            <div className="">
+
+            </div>
+        );
+    }
+}
