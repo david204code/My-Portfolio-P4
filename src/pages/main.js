195c6734b21cbc8c777da2f2950a644f66be1a19
David Wu <david204code@gmail.com>
2018-06-16 21:17:48 +0100

    main page lay out

--- src/pages/main.js
+++ src/pages/main.js
@@ -1,13 +1,20 @@
 import React from 'react';
+import './main.css'
 
 
 class Main extends React.Component {
     render() {
         return (
             <div className="main">
-                
+                <div className="menu">    
+                    <div className="projects">
+                        Hello
+                    </div>
+                </div>
+                <div className="introduction">
+                    Wu
+                </div>
             </div>
-
         );
     }
 }
